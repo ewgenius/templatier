0dfb9b97fb4cb7515265a4020ef41d328840bdf2
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-02-07 23:35:43 +0300

    template input

--- dist/index.js
+++ dist/index.js
@@ -7,35 +7,73 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const path = require("path");
+const fs_1 = require("fs");
 const program = require("commander");
 const inquirer = require("inquirer");
+const handlebars_1 = require("handlebars");
 const packjson = require('../package.json');
+function loadTemplate(path) {
+    try {
+        const config = require(`${path}/templatier.json`);
+        return config;
+    }
+    catch (error) {
+        return null;
+    }
+}
+function readFilePromise(filename) {
+    return new Promise((resolve, reject) => {
+        fs_1.readFile(filename, 'utf8', (err, data) => {
+            if (err)
+                reject(err);
+            else
+                resolve(data);
+        });
+    });
+}
+function compileTemplate(basePath, config) {
+    Promise.all(config.templates.map(template => {
+        return Promise.all(Object.keys(template.variables || {}).map(key => {
+            console.log(key);
+            return inquirer.prompt([{
+                    type: 'input',
+                    name: key,
+                    message: `Enter ${key}`
+                }]);
+        })).then(variables => {
+            console.log(variables);
+            return readFilePromise(path.resolve(basePath, template.path))
+                .then(input => handlebars_1.compile(input))
+                .then((compiled) => compiled({}));
+        });
+    }));
+}
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         program
             .version(packjson.version)
             .option('-t, --template <template-name>', 'specify template name', null)
-            .option('-n, --name <file-name>', 'specify file name', null)
+            .option('-p, --path <dest-path>', 'specify path', './')
             .parse(process.argv);
-        let { template, name } = program;
+        let { template } = program;
         if (!template) {
             const result = yield inquirer.prompt([{
-                    type: 'input',
+                    type: 'list',
                     name: 'template',
-                    message: 'Enter template name'
+                    message: 'Enter template name',
+                    choices: [
+                        'templatier-test',
+                        'templatier-react'
+                    ]
                 }]);
             template = result.template;
         }
-        if (!name) {
-            const result = yield inquirer.prompt([{
-                    type: 'input',
-                    name: 'name',
-                    message: 'Enter file name'
-                }]);
-            name = result.name;
+        const configPath = path.resolve(__dirname, '../templates', template);
+        const config = loadTemplate(configPath);
+        if (config) {
+            console.log(compileTemplate(configPath, config));
         }
-        console.log(name);
-        console.log(template);
     });
 }
 run();
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,87 @@
+import * as path from 'path';
+import { readFile } from 'fs';
 import * as program from 'commander';
 import * as inquirer from 'inquirer';
+import { compile, parse } from 'handlebars';
 const packjson = require('../package.json');
 
 interface TemplatierArguments {
   template: string | null;
-  name: string | null;
+}
+
+interface TemplatierTemplate {
+  name: string;
+  extension: string;
+  path: string;
+  variables: string[];
+}
+
+interface TemplatierConfig {
+  templates: TemplatierTemplate[];
+}
+
+function loadTemplate(path: string): TemplatierConfig | null {
+  try {
+    const config: TemplatierConfig = require(`${path}/templatier.json`);
+    return config;
+  } catch (error) {
+    return null;
+  }
+}
+
+function readFilePromise(filename: string) {
+  return new Promise((resolve, reject) => {
+    readFile(filename, 'utf8', (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
+
+function compileTemplate(basePath: string, config: TemplatierConfig) {
+  Promise.all(config.templates.map(template => {
+    return Promise.all(Object.keys(template.variables || {}).map(key => {
+      return inquirer.prompt([{
+        type: 'input',
+        name: key,
+        message: `Enter ${key}`
+      }]);
+    })).then(variables => {
+      console.log(variables);
+      return readFilePromise(path.resolve(basePath, template.path))
+        .then(input => compile(input))
+        .then((compiled: any) => compiled({}));
+    });
+  }));
 }
 
 async function run() {
   program
     .version(packjson.version)
     .option('-t, --template <template-name>', 'specify template name', null)
-    .option('-n, --name <file-name>', 'specify file name', null)
+    .option('-p, --path <dest-path>', 'specify path', './')
     .parse(process.argv);
 
-  let {template, name} = (program as any) as TemplatierArguments;
+  let {template} = (program as any) as TemplatierArguments;
 
   if (!template) {
     const result = await inquirer.prompt([{
-      type: 'input',
+      type: 'list',
       name: 'template',
-      message: 'Enter template name'
+      message: 'Enter template name',
+      choices: [
+        'templatier-test',
+        'templatier-react'
+      ]
     }]);
     template = (result as any).template;
   }
 
-  if (!name) {
-    const result = await inquirer.prompt([{
-      type: 'input',
-      name: 'name',
-      message: 'Enter file name'
-    }]);
-    name = (result as any).name;
+  const configPath = path.resolve(__dirname, '../templates', template);
+  const config = loadTemplate(configPath);
+  if (config) {
+    console.log(compileTemplate(configPath, config));
   }
-
-  console.log(name);
-  console.log(template);
 }
 
 run();
