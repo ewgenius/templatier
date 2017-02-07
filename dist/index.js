ed66635ca351c4db619ea06e7fd8e6de1db00ac8
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-02-07 22:11:17 +0300

    base cli input

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,40 @@
-console.log('init');
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const program = require("commander");
+const inquirer = require("inquirer");
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        program
+            .version('1.0.0')
+            .option('-t, --template <template-name>', 'specify template name', null)
+            .option('-n, --name <file-name>', 'specify file name', null)
+            .parse(process.argv);
+        let { template, name } = program;
+        if (!template) {
+            const result = yield inquirer.prompt([{
+                    type: 'input',
+                    name: 'template',
+                    message: 'Enter template name'
+                }]);
+            template = result.template;
+        }
+        if (!name) {
+            const result = yield inquirer.prompt([{
+                    type: 'input',
+                    name: 'name',
+                    message: 'Enter file name'
+                }]);
+            name = result.name;
+        }
+        console.log(name);
+        console.log(template);
+    });
+}
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log('init');
+import * as program from 'commander';
+import * as inquirer from 'inquirer';
+
+interface TemplatierArguments {
+  template: string | null;
+  name: string | null;
+}
+
+async function run() {
+  program
+    .version('1.0.0')
+    .option('-t, --template <template-name>', 'specify template name', null)
+    .option('-n, --name <file-name>', 'specify file name', null)
+    .parse(process.argv);
+
+  let {template, name} = (program as any) as TemplatierArguments;
+
+  if (!template) {
+    const result = await inquirer.prompt([{
+      type: 'input',
+      name: 'template',
+      message: 'Enter template name'
+    }]);
+    template = (result as any).template;
+  }
+
+  if (!name) {
+    const result = await inquirer.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Enter file name'
+    }]);
+    name = (result as any).name;
+  }
+
+  console.log(name);
+  console.log(template);
+}
+
+run();
